82c58ab59bf7b437f9e934725b753413454eb152
Eduardo Lupinetti Bandeira <eduardo.lupi@outlook.com>
2024-05-22 18:33:56 -0300

    up

---
+++ Exercises/Print formatting.py
@@ -0,0 +1,3 @@
+#Write an expression using any of the string formatting methods we have learned (except f-strings, see note below) to return the phrase 'Python rules!' 
+
+print("{} {}".format('Python', 'rules!'))
